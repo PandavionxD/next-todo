5aa5e8cff6714e702cb4ee594ab2870d742057f8
Alex Daniel <1151372@senati.pe>
2024-05-17 22:37:08 -0500

    Terminando de crear semillas.

---
+++ src/app/api/seed/route.ts
@@ -0,0 +1,30 @@
+import prisma from "@/lib/prisma";
+import { NextResponse, NextRequest } from "next/server";
+
+export async function GET(request: Request) {
+  // Crear un solo dato con la funcion crear
+  //   const todo1 = await prisma.todo.create({
+  //     data: {
+  //       description: "Piedra del alma",
+  //     },
+  //   });
+
+  await prisma.todo.deleteMany()
+
+  const todo1 = await prisma.todo.createMany({
+    data: [
+      { description: "piedra del alma" },
+      { description: "piedra del mente" },
+      { description: "piedra del espacio", complete: true },
+      { description: "piedra del poder" },
+      { description: "piedra del tiempo" },
+      { description: "piedra del realidad" },
+    ],
+  });
+
+  console.log({ todo1 });
+
+  return NextResponse.json({
+    message: "Semillas creadas",
+  });
+}
